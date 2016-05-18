453b935c1bbda6f470c39bd55b3d9152c43501c2
Serhiy <serhiyhulyi93@gmail.com>
2016-05-18 19:13:14 +0300

    dsf

--- js/main.js
+++ js/main.js
@@ -8,44 +8,94 @@ window.params = {
 
 jQuery(document).ready(function($) {
 
-/*---------------------------
-                              ADD CLASS ON SCROLL
----------------------------*/
-$(function() { 
+  /*---------------------------
+                                ADD CLASS ON SCROLL
+  ---------------------------*/
+  $(function() { 
   var $document = $(document),
       $element = $('.menu-button'),
       $element2 = $('header'),
       className = 'hasScrolled';
 
-  $document.scroll(function() {
-    $element.toggleClass(className, $document.scrollTop() >= 1);
-    $element2.toggleClass(className, $document.scrollTop() >= 1);
+    $document.scroll(function() {
+      $element.toggleClass(className, $document.scrollTop() >= 1);
+      $element2.toggleClass(className, $document.scrollTop() >= 1);
+    });
   });
-});
 
 
-/*---------------------------
-                              MENU TOGGLE
----------------------------*/
-$('.menu-button').on('click', function(event) {
-  event.preventDefault();
-  $(this).toggleClass('active');
-  $(this).siblings('header').toggleClass('active');
-  if ($('header').hasClass('active')) {
-      $('body, html').css('overflow', 'hidden');
-    } else {
-      $('body, html').css('overflow', 'visible');
-    }
-});
+  /*---------------------------
+                                MENU TOGGLE
+  ---------------------------*/
+  $('.menu-button').on('click', function(event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $(this).siblings('header').toggleClass('active');
+    if ($('header').hasClass('active')) {
+        $('body, html').css('overflow', 'hidden');
+      } else {
+        $('body, html').css('overflow', 'visible');
+      }
+  });
 
 
-$('.offer__slider').slick({
-  dots: true,
-  arrows: false,
-  fade: true
-});
+  $('.offer__slider').slick({
+    dots: true,
+    arrows: false,
+    fade: true
+  });
 
-});
+  $('.clients__slider').slick({
+    dots: false,
+    arrows: true,
+    lazyLoad: 'ondemand',
+    slidesToShow: 5,
+    slidesToScroll: 1,
+
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+  });
+
+  $('.autopark__slider').slick({
+    dots: false,
+    arrows: true,
+    lazyLoad: 'ondemand',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 970,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+}); // end
 
 
 
