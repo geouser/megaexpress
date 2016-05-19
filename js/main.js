043b7d4b22d4584962916a5d1f6b06043148149e
OlehRusyi <olehrusy@gmail,com>
2016-05-19 22:21:59 +0300

    сів

--- js/main.js
+++ js/main.js
@@ -163,6 +163,54 @@ $('a.insert-more').on('click', function(event) {
 });
 
 
+/*---------------------------
+                              GOOGLE MAP
+---------------------------*/
+  var map;
+  function googleMap_initialize() {
+    var lat = $('#map_canvas').data('lat');
+    var long = $('#map_canvas').data('long');
+    var mapCenterCoord = new google.maps.LatLng(lat+0.001, long+0.006);
+    var mapMarkerCoord = new google.maps.LatLng(lat, long);
+    if ( $(window).width() < 768 ) {
+      mapCenterCoord = new google.maps.LatLng(lat, long);
+      mapMarkerCoord = new google.maps.LatLng(lat, long);
+    }
+    $(window).resize(function(event) {
+      if ( $(window).width() < 768 ) {
+        mapCenterCoord = new google.maps.LatLng(lat, long);
+        mapMarkerCoord = new google.maps.LatLng(lat, long);
+      } else {
+        mapCenterCoord = new google.maps.LatLng(lat+0.001, long+0.006);
+        mapMarkerCoord = new google.maps.LatLng(lat, long);
+      }
+    });
+
+      var mapOptions = {
+      center: mapCenterCoord,
+      zoom: 15,
+      //draggable: false,
+      disableDefaultUI: true,
+      scrollwheel: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    var markerImage = new google.maps.MarkerImage('images/location.png');
+    var marker = new google.maps.Marker({
+      icon: markerImage,
+      position: mapMarkerCoord, 
+      map: map,
+      title:"Megaexpress"
+    });
+    
+    $(window).resize(function (){
+      map.setCenter(mapCenterCoord);
+    });
+  }
+
+  googleMap_initialize(); 
+
 
 }); // end
 
